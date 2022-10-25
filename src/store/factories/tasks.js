0ac87b3553ba2c4c9e7b31fc24594a851566e075
MAZE <remvze@proton.me>
2022-10-25 13:00:37 -0700

    feat: add persistance

--- src/store/factories/tasks.js
+++ src/store/factories/tasks.js
@@ -1,132 +1,129 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 import { v4 as uuid } from 'uuid';
 
 const createTasks = () =>
-  create((set, get) => ({
-    tasks: [
-      {
-        id: uuid(),
-        text: 'Buy some milk',
-        done: true,
-      },
-      {
-        id: uuid(),
-        text: 'Buy some bread',
-        done: false,
-      },
-    ],
-    pins: [
-      {
-        id: uuid(),
-        text: 'Buy some bread',
-        done: false,
-      },
-    ],
-
-    check(id, pinned = false) {
-      const key = pinned ? 'pins' : 'tasks';
-
-      set(state => ({
-        [key]: state[key].map(task => {
-          if (task.id !== id) return task;
-
-          task.done = !task.done;
-
-          return task;
-        }),
-      }));
-    },
-
-    write(id, text, pinned = false) {
-      const key = pinned ? 'pins' : 'tasks';
-
-      set(state => ({
-        [key]: state[key].map(task => {
-          if (task.id !== id) return task;
-
-          task.text = text;
-          task.done = text.length === 0 ? false : task.done;
-
-          return task;
-        }),
-      }));
-    },
-
-    add(id, pinned = false) {
-      const key = pinned ? 'pins' : 'tasks';
-
-      const state = get();
-      const index = state[key].findIndex(task => task.id === id);
-      const next = state[key][index + 1];
-      const newID = uuid();
-
-      if (next?.text?.length === 0) return next.id;
-
-      set(state => {
-        const tasks = [...state[key]];
-
-        tasks.splice(index + 1, 0, {
-          id: newID,
-          text: '',
+  create(
+    persist((set, get) => ({
+      tasks: [
+        {
+          id: uuid(),
+          text: 'Make yourself a better person',
+          done: true,
+        },
+        {
+          id: uuid(),
+          text: 'Make the world a better place',
           done: false,
+        },
+      ],
+      pins: [],
+
+      check(id, pinned = false) {
+        const key = pinned ? 'pins' : 'tasks';
+
+        set(state => ({
+          [key]: state[key].map(task => {
+            if (task.id !== id) return task;
+
+            task.done = !task.done;
+
+            return task;
+          }),
+        }));
+      },
+
+      write(id, text, pinned = false) {
+        const key = pinned ? 'pins' : 'tasks';
+
+        set(state => ({
+          [key]: state[key].map(task => {
+            if (task.id !== id) return task;
+
+            task.text = text;
+            task.done = text.length === 0 ? false : task.done;
+
+            return task;
+          }),
+        }));
+      },
+
+      add(id, pinned = false) {
+        const key = pinned ? 'pins' : 'tasks';
+
+        const state = get();
+        const index = state[key].findIndex(task => task.id === id);
+        const next = state[key][index + 1];
+        const newID = uuid();
+
+        if (next?.text?.length === 0) return next.id;
+
+        set(state => {
+          const tasks = [...state[key]];
+
+          tasks.splice(index + 1, 0, {
+            id: newID,
+            text: '',
+            done: false,
+          });
+
+          return { [key]: tasks };
         });
 
-        return { [key]: tasks };
-      });
+        return newID;
+      },
 
-      return newID;
-    },
+      remove(id, pinned = false) {
+        const key = pinned ? 'pins' : 'tasks';
 
-    remove(id, pinned = false) {
-      const key = pinned ? 'pins' : 'tasks';
+        const state = get();
+        const index = state[key].findIndex(task => task.id === id);
+        const prev = state[key][index - 1];
 
-      const state = get();
-      const index = state[key].findIndex(task => task.id === id);
-      const prev = state[key][index - 1];
+        if (key === 'tasks' && state.tasks.length === 1) throw new Error();
 
-      if (key === 'tasks' && state.tasks.length === 1) throw new Error();
+        set(state => {
+          const tasks = [...state[key]];
 
-      set(state => {
-        const tasks = [...state[key]];
+          tasks.splice(index, 1);
 
-        tasks.splice(index, 1);
+          return { [key]: tasks };
+        });
 
-        return { [key]: tasks };
-      });
+        return prev?.id || null;
+      },
 
-      return prev?.id || null;
-    },
+      reorder(tasks, pinned = false) {
+        const key = pinned ? 'pins' : 'tasks';
 
-    reorder(tasks, pinned = false) {
-      const key = pinned ? 'pins' : 'tasks';
+        set(() => ({ [key]: tasks }));
+      },
 
-      set(() => ({ [key]: tasks }));
-    },
+      togglePin(id) {
+        const state = get();
+        const isPinned = state.pins.some(task => task.id === id);
+        const from = isPinned ? 'pins' : 'tasks';
+        const to = isPinned ? 'tasks' : 'pins';
 
-    togglePin(id) {
-      const state = get();
-      const isPinned = state.pins.some(task => task.id === id);
-      const from = isPinned ? 'pins' : 'tasks';
-      const to = isPinned ? 'tasks' : 'pins';
+        console.log('hi');
 
-      console.log('hi');
+        set(state => {
+          const values = {
+            tasks: [...state.tasks],
+            pins: [...state.pins],
+          };
 
-      set(state => {
-        const values = {
-          tasks: [...state.tasks],
-          pins: [...state.pins],
-        };
+          const index = values[from].findIndex(task => task.id === id);
+          const [task] = values[from].splice(index, 1);
 
-        const index = values[from].findIndex(task => task.id === id);
-        const [task] = values[from].splice(index, 1);
+          values[to].push(task);
 
-        values[to].push(task);
+          console.log(values);
 
-        console.log(values);
-
-        return values;
-      });
-    },
-  }));
+          return values;
+        });
+      },
+    }))
+  );
 
 export default createTasks;
