3e64387bc19af49f5e27497fe511d751cedf4128
MAZE <remvze@proton.me>
2022-10-25 10:11:57 -0700

    refactor: pascal case components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from '@/components/app';
+import App from '@/components/App';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
