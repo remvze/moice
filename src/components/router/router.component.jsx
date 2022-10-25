afe6a6a3b20035a1d7891b45907e0483d0a49e1d
MAZE <remvze@proton.me>
2022-10-25 06:21:52 -0700

    feat: add not found page

--- src/components/router/router.component.jsx
+++ src/components/router/router.component.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from '@/pages/home';
+import NotFound from '@/pages/not-found';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
