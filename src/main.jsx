de32392b2bdac49a24b2df388ccfbd482fb92e16
MAZE <remvze@proton.me>
2022-10-25 05:55:59 -0700

    feat: reset the base

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from '@/components/app';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
