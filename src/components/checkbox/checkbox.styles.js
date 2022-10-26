91566221f3f08279be5536c128587be3f4f4dd88
MAZE <remvze@proton.me>
2022-10-26 03:43:43 -0700

    style: add shadow to checkbox

--- src/components/checkbox/checkbox.styles.js
+++ src/components/checkbox/checkbox.styles.js
@@ -7,17 +7,19 @@ export const Container = styled.label`
 
 export const Box = styled.div`
   display: inline-block;
-  width: 20px;
-  height: 20px;
+  width: 17px;
+  height: 17px;
   border: 2px solid #888;
   border-radius: 5px;
   cursor: pointer;
   background-color: ${({ theme }) => theme.colors.background};
+  transition: box-shadow 0.4s;
 
   &.checked {
     border: 0;
     background-color: #b6e388;
     background-image: linear-gradient(45deg, #b6e388, #c7f2a4);
+    box-shadow: 0 4px 10px rgb(182, 227, 136, 0.44);
   }
 `;
 
