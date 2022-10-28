82ca2c30f0ac6fbfdf698f9501cff1d99bed22bc
MAZE <remvze@proton.me>
2022-10-28 07:18:35 -0700

    feat: change the page title

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,16 +1,24 @@
+import { Helmet } from 'react-helmet';
+
 import MainLayout from '@/layouts/main';
 import * as S from './not-found.styles';
 
 const NotFound = () => {
   return (
-    <MainLayout>
-      <S.Wrapper>
-        <div>
-          <S.Code>404</S.Code>
-          <S.Message>Ops! Page not found.</S.Message>
-        </div>
-      </S.Wrapper>
-    </MainLayout>
+    <>
+      <Helmet>
+        <title>Moice: Page Not Found</title>
+      </Helmet>
+
+      <MainLayout>
+        <S.Wrapper>
+          <div>
+            <S.Code>404</S.Code>
+            <S.Message>Ops! Page not found.</S.Message>
+          </div>
+        </S.Wrapper>
+      </MainLayout>
+    </>
   );
 };
 
