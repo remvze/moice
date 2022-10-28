2562871491554b3f6ffc9cf2a4d8fb23a4181d41
MAZE <remvze@proton.me>
2022-10-28 06:39:23 -0700

    docs: add jsdoc for function

--- src/helpers/wait.js
+++ src/helpers/wait.js
@@ -1,3 +1,12 @@
+/**
+ * Wait until a function returns true
+ *
+ * @param {Function} func - Verification function
+ * @returns {Promise}
+ *
+ * @example
+ *   await until(() => Math.random() > 0.8)
+ */
 export const until = func =>
   new Promise(resolve => {
     const check = () => {
