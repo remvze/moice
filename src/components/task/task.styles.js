16eb329240c31e0c23fed16f919ce00f898ff1a8
MAZE <remvze@proton.me>
2022-11-10 03:08:33 -0800

    style: remove margin from the last task

--- src/components/task/task.styles.js
+++ src/components/task/task.styles.js
@@ -4,8 +4,11 @@ import { Reorder } from 'framer-motion';
 export const Wrapper = styled(Reorder.Item)`
   display: flex;
   align-items: flex-start;
-  margin-bottom: 15px;
   position: relative;
   overflow: hidden;
   user-select: none;
+
+  &:not(:last-of-type) {
+    margin-bottom: 15px;
+  }
 `;
