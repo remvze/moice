3215570c5796018c58acb45f69e697661209a67d
MAZE <remvze@proton.me>
2022-11-10 02:13:52 -0800

    style: fix empty state color

--- src/components/empty-message/empty-message.styles.js
+++ src/components/empty-message/empty-message.styles.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 export const Message = styled(motion.p)`
-  color: ${({ theme }) => theme.fontSize.s};
+  color: ${({ theme }) => theme.colors.text[800]};
   font-size: ${({ theme }) => theme.fontSize.s};
 `;
