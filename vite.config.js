03ef6388b4924a567a896ac8e8b83085b0b69720
MAZE <remvze@proton.me>
2022-11-18 07:08:40 -0800

    perf: add lazy load to deps

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { dependencies } from './package.json';
+
+const renderChunks = deps => {
+  const chunks = {};
+
+  Object.keys(deps).forEach(key => {
+    if (['react', 'react-dom', 'react-router-dom'].includes(key)) return;
+
+    chunks[key] = [key];
+  });
+
+  return chunks;
+};
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,5 +25,16 @@ export default defineConfig({
         replacement: path.resolve(__dirname, 'src'),
       },
     ],
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 });
