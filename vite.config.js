7ab1e05dcf476ed75edb1fd42833a7ab5b9eed8b
MAZE <remvze@proton.me>
2022-11-25 02:51:36 -0800

    feat: implement basic pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { dependencies } from './package.json';
 
@@ -17,7 +18,36 @@ const renderChunks = deps => {
 };
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Moice',
+        short_name: 'Moice',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#080808',
+        theme_color: '#080808',
+        lang: 'en',
+        scope: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
